45cb0b513985d3422b825c438ee3fe3a50477adc
NoamTsabary <Noam@sabarilaw.com>
2022-06-19 15:14:39 +0300

    Exercise 1-5

--- D01/EX05/index.js
+++ D01/EX05/index.js
@@ -1,5 +1,11 @@
-function moduloEx5() {
-  //your code
+function moduloEx5(num, comparer) {
+  let text;
+  if (num > comparer && (num%comparer) !== 0) text = `The number ${num} is bigger than ${comparer}. But the modulo of ${num} % ${comparer} is ${num%comparer}`;
+  else if (num > comparer) text =`The number ${num} is bigger than ${comparer}`;
+  else if (num === comparer) text =`The number ${num} is equal to ${comparer}`;
+  else if (num < comparer) text =`The number ${num} is less than ${comparer}`;  
+
+  console.log(text);
 }
 
 // Do not remove or change this line, or the tests won't work
