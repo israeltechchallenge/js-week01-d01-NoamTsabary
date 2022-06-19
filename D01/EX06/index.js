c8042b502349187cde59a0e9b6462c23e2585a53
NoamTsabary <Noam@sabarilaw.com>
2022-06-19 17:03:58 +0300

    ex06

--- D01/EX06/index.js
+++ D01/EX06/index.js
@@ -1,3 +1,14 @@
-function moduloEx6(number, comparer) {
-  //your code
+function moduloEx6(num, comparer) {
+  
+  let text;
+  if (comparer === 0 && num !== 0) text = `The number ${num} is bigger than ${comparer}. But the modulo of anyNumber % ${comparer} is Not a Number`;
+  else if (num > comparer && (num%comparer) !== 0) text = `The number ${num} is bigger than ${comparer}. But the modulo of ${num} % ${comparer} is ${num%comparer}`;
+  else if (num > comparer) text =`The number ${num} is bigger than ${comparer}`;
+  else if (num === comparer) text =`The number ${num} is equal to ${comparer}`;
+  else if (num < comparer) text =`The number ${num} is less than ${comparer}`;  
+
+  document.querySelector("h3").innerText = text;
+
 }
+
+moduloEx6(4, 2)
